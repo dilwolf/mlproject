fd12bf8970c04b65fe87ad8d91cc8efc43a71022
dilwolf <dilshodbazarov007@gmail.com>
2024-04-16 22:03:43 +0900

    added setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "mlproject",
+    version = "0.0.1",
+    author = "dilwolf",
+    author_email= "dilshodbazarov007@gmail.com",
+    packages = find_packages(),
+    install_requires = ['pandas', 'numpy']
+
+)
